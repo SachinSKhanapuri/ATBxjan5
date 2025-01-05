06b4e13b2a6b9c8c2dc0fde4647ca830c840fdb7
SachinSKhanapuri <khanapurisachin@gmail.com>
2025-01-05 11:46:51 +0530

    Initial commit

---
+++ src/jan5task.java
@@ -0,0 +1,6 @@
+public class jan5task {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java Day 2");
+    }
+}
---
+++ src/p2_println_vs_print.java
@@ -0,0 +1,9 @@
+public class p2_println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("hello 1");
+        System.out.println("hello 2");
+
+        System.out.print("hello 3");
+        System.out.print("Hello 4");
+    }
+}
